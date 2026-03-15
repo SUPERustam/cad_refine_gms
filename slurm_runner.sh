25dccd2838f30831ee7b9d516bc21aee779dc224
Rustam Uzdenov <498rustam@gmail.com>
2026-03-15 10:04:06 +0000

    🤒 Second resume

--- slurm_runner.sh
+++ slurm_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=cadrille_rl
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=30
 #SBATCH --gpus=4
 #SBATCH --output=slurm_logs/slurm_log-%j.out
 
-srun bash /scratch/498rustam/cad_refine_m/train_loop_dp_gms_resume.sh
+srun bash /scratch/498rustam/cad_refine_m/train_loop_dp_gms_resume_2.sh
